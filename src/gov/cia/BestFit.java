d36720cb0610b290c22a8345c933e0db4e1c64f7
Lucas Churchill <lucas@agtlucas.com>
2014-09-13 16:00:53 -0300

    Update BestFit

--- src/gov/cia/BestFit.java
+++ src/gov/cia/BestFit.java
@@ -16,6 +16,49 @@ public class BestFit {
 		System.out.println("Número de blocos de memória: ");
 		mem = in.nextInt();
 		
+		int tamProcessos[] = new int[proc];
+		int segMemoria[] = new int[mem];
+		
+		int i;
+		
+		for (i = 0; i < proc; i++) {
+			System.out.println("Tamanho do processo " + (i + 1));
+			tamProcessos[i] = in.nextInt();
+		}
+		
+		for (i = 0; i < mem; i++) {
+			System.out.println("Entre o tamanho do segmento de memória: " + (i + 1));
+			segMemoria[i] = in.nextInt();
+		}
+		
+		int j;
+		
+		for (i = 0; i < mem; i++) {
+			for (j = i + 1; j < mem; j++) {
+				if (segMemoria[i] > segMemoria[j]) {
+					int t = segMemoria[i];
+					segMemoria[i] = segMemoria[j];
+					segMemoria[j] = t;
+				}
+			}
+		}
+		
+		for (i = 0; i < proc; i++) {
+			for (j = 0; j < mem; j++) {
+				if (segMemoria[j] >= tamProcessos[i]) {
+					segMemoria[j] -= tamProcessos[i];
+					System.out.println("Process " + (i + 1) + " alocado no espaço de memória " + (j + 1));
+					System.out.println("Espaço restante após a alocação " + segMemoria[j]);
+					System.out.println("AEHOOOOOO");
+					break;
+				}
+			}
+			if (j == mem) {
+				System.out.println("Erro! Acabou! Não há mais memória para o processo " + i);
+				break;
+			}
+		}
+		
 	}
 
 }
