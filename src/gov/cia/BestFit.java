2e6d4cf0627ff4c4851df10bff19d3a553f730bc
Lucas Churchill <lucas@agtlucas.com>
2014-09-13 15:40:22 -0300

    Adding BestFit

---
+++ src/gov/cia/BestFit.java
@@ -0,0 +1,21 @@
+package gov.cia;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class BestFit {
+	
+	public static void main(String[] args) {
+		
+		int proc, mem;
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Número de processos: ");
+		proc = in.nextInt();
+		System.out.println("Número de blocos de memória: ");
+		mem = in.nextInt();
+		
+	}
+
+}
